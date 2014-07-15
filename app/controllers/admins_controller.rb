3acc212ce49bfc67c317fa895e500ee903960809
vania-ermackovec <vania.ermackovec@gmail.com>
2014-07-15 17:33:21 +0300

    commit

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -4,10 +4,12 @@ class AdminsController < ApplicationController
 
   def users
     @users = User.all.page(params[:page]).per_page(10)
+    @fwr
   end
 
   def all_ads
      @adss = Advertisement.without_draft.time_post_order.page(params[:page]).per_page(20)
+     @dsf
   end
 
   def categories
